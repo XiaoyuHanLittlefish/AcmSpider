e3c4550281ae08f8de71c2019dddc73d38bc7113
469714875@qq.com <469714875@qq.com>
2022-11-04 20:32:59 +0800

    创建了VjudgeContestItem类

--- AcmSpider/items.py
+++ AcmSpider/items.py
@@ -17,3 +17,37 @@ class NowcoderItem(scrapy.Item):
     name = scrapy.Field()
     rating = scrapy.Field()
     solve_problem_number = scrapy.Field()
+
+
+class VjudgeContestItem(scrapy.Item):
+    class Problem():
+        problem_no = 'A'
+        problem_name = ''
+        is_solved = False
+        solved_time = 0
+        attempted_time = 0
+
+        def __init__(self, problem_no, problem_name, is_solved, solved_time, attempted_time):
+            self.problem_no = problem_no
+            self.problem_name = problem_name
+            self.is_solved = is_solved
+            self.solved_time = solved_time
+            self.attempted_time = attempted_time
+
+    class Team():
+        name = 'Default Name'
+        solved_problem_num = 0
+        rank = 0
+        score = 0
+        penalty = 0
+        problems = []
+
+        def __init__(self, name, solved_problem_num, score, penalty):
+            self.name = name
+            self.solved_problem_num = solved_problem_num
+            self.score = score
+
+    teams = scrapy.Field()
+
+
+
